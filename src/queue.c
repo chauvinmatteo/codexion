c6ec5b3244de1935f16a1a6be9e371af0c22636e
Matteo Chauvin <mchauvin@z1r12p1.42lyon.fr>
2026-05-18 11:13:49 +0200

    dev(queue): WIP queue system

---
+++ src/queue.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchauvin <mchauvin@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 10:46:21 by mchauvin          #+#    #+#             */
+/*   Updated: 2026/05/13 14:22:43 by mchauvin         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/codexion.h"
+
+int	ft_enqueue(t_queue *queue, t_coders *coder)
+{
+	t_ticket	*new_coder;
+
+	new_coder = malloc(sizeof(t_ticket));
+	if (!new_coder)
+		return (-1);
+	new_coder->coders = coder;
+	new_coder->next = NULL;
+	pthread_mutex_lock(&queue->queue_lock);
+	if (queue->head == NULL)
+	{
+		queue->head = new_coder;
+		queue->tail = new_coder;
+	}
+	else
+	{
+		queue->tail->next = new_coder;
+		queue->tail = new_coder;
+	}
+	queue->size++;
+	pthread_mutex_unlock(&queue->queue_lock);
+	return (0);
+}
+
+t_coders	*ft_dequeue(t_queue *queue, char *scheduler)
+{
+	t_ticket	*prev;
+	t_ticket	*target;
+	t_coders	*coder;
+
+	pthread_mutex_lock(&queue->queue_lock);
+	if (queue->size == 0)
+	{
+		pthread_mutex_unlock(&queue->queue_lock);
+		return (NULL);
+	}
+	target = fifo(queue, &prev);
+	if (prev == NULL)
+		queue->head = target->next;
+	else
+		prev->next = target->next;
+	if (target == queue->tail)
+        queue->tail = prev;
+	coder = target->coders;
+	free(target);
+	queue->size--;
+	pthread_mutex_unlock(&queue->queue_lock);
+	return (coder);
+}

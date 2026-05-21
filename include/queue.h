4f4ecef679fb37fb7275138487dde87456588fa2
Matteo Chauvin <mchauvin@z2r11p5.42lyon.fr>
2026-05-21 10:08:33 +0200

    fix(library): added enqueue and dequeue in .h

--- include/queue.h
+++ include/queue.h
@@ -6,7 +6,7 @@
 /*   By: mchauvin <mchauvin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 10:33:04 by mchauvin          #+#    #+#             */
-/*   Updated: 2026/05/18 11:18:07 by mchauvin         ###   ########lyon.fr   */
+/*   Updated: 2026/05/18 15:44:15 by mchauvin         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,6 @@ typedef struct s_queue
 }					t_queue;
 
 t_ticket			*fifo(t_queue *queue, t_ticket **prev);
+int					ft_enqueue(t_queue *queue, t_coders *coder);
+t_coders			*ft_dequeue(t_queue *queue, char *scheduler);
 #endif

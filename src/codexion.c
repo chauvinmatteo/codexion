4137957f1c516fe333045d34fdfa0f722881c7eb
Matteo Chauvin <mchauvin@z2r11p7.42lyon.fr>
2026-05-22 11:15:46 +0200

    chores(main): normed codexion.h

--- src/codexion.c
+++ src/codexion.c
@@ -6,20 +6,20 @@
 /*   By: mchauvin <mchauvin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 14:38:27 by mchauvin          #+#    #+#             */
-/*   Updated: 2026/05/12 16:24:21 by mchauvin         ###   ########lyon.fr   */
+/*   Updated: 2026/05/21 13:30:26 by mchauvin         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/codexion.h"
 
-int ft_simulation_state(t_codex *data)
+int	ft_simulation_state(t_codex *data)
 {
-    int status;
+	int	status;
 
-    pthread_mutex_lock(&data->state_lock);
-    status = data->running;
-    pthread_mutex_unlock(&data->state_lock);
-    return status;
+	pthread_mutex_lock(&data->state_lock);
+	status = data->running;
+	pthread_mutex_unlock(&data->state_lock);
+	return (status);
 }
 
 int	main(int ac, char **av)
@@ -30,7 +30,6 @@ int	main(int ac, char **av)
 		return (1);
 	if (init_codexion(&codex) == -1)
 		return (1);
-    
 	start_codexion(&codex);
 	free_codexion(&codex);
 	return (0);
